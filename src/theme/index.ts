9d260151f178ed3614f511155db57a7850b556e2
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2021-05-25 03:22:02 +0100

    Added typography in theme

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,9 +1,11 @@
 import colors from './colors';
+import typography from './typography';
 import zIndex from './zIndex';
 
 const theme = {
   colors,
   spacing: (x: number): number => x * 8,
+  typography,
   zIndex,
 };
 
---
+++ src/theme/typography.ts
@@ -0,0 +1,15 @@
+const typography = {
+  nodeHeader: () => `
+    font-weight: 900;
+    font-size: 8px;
+    line-height: 12px;
+    text-transform: uppercase;
+  `,
+  nodeBody: () => `
+    font-weight: normal;
+    font-size: 16px;
+    line-height: 24px;
+  `,
+};
+
+export default typography;
