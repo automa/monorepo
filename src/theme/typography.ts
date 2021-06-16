fd5ca9385bf77b542d7cc516197b7b289393ea22
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2021-06-16 08:37:13 +0100

    Added type for theme

--- src/theme/typography.ts
+++ src/theme/typography.ts
@@ -1,11 +1,5 @@
 const typography = {
-  nodeHeader: () => `
-    font-weight: 900;
-    font-size: 8px;
-    line-height: 12px;
-    text-transform: uppercase;
-  `,
-  nodeBody: () => `
+  body: () => `
     font-weight: normal;
     font-size: 16px;
     line-height: 24px;
