1b6cfa9118fb371bebdcf70f0ebdcaa189555d3c
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2021-10-30 12:23:37 +0100

    Add App.styles.ts back

--- src/views/App/App.tsx
+++ src/views/App/App.tsx
@@ -5,14 +5,17 @@ import Loader from 'components/Loader';
 import RoutesLoader from 'components/RoutesLoader';
 
 import routes from './routes';
+import { Container } from './App.styles';
 
 export interface AppProps {}
 
 const App: React.FC<AppProps> = () => {
   return (
-    <BrowserRouter>
-      <RoutesLoader fallback={<Loader />} routes={routes} />
-    </BrowserRouter>
+    <Container>
+      <BrowserRouter>
+        <RoutesLoader fallback={<Loader />} routes={routes} />
+      </BrowserRouter>
+    </Container>
   );
 };
 
