bd6e7cf083a6e01605f04a23e5d99df87b0945e4
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2022-02-23 15:41:21 +0000

    Added testing, fixes #24

---
+++ src/tests.tsx
@@ -0,0 +1,18 @@
+import React, { ReactElement } from 'react';
+import { ThemeProvider } from 'styled-components';
+import { render, RenderOptions } from '@testing-library/react';
+
+import theme from 'theme';
+
+const AllTheProviders: React.FC = ({ children }) => {
+  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+};
+
+const customRender = (
+  ui: ReactElement,
+  options?: Omit<RenderOptions, 'wrapper'>,
+) => render(ui, { wrapper: AllTheProviders, ...options });
+
+export * from '@testing-library/react';
+
+export { customRender as render };
