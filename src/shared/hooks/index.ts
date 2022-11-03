7cace15588e063a2f2f97da133831d9268dae5f6
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2022-11-03 00:58:51 +0000

    Added useAsyncEffect & useIsMounted hooks

--- src/shared/hooks/index.ts
+++ src/shared/hooks/index.ts
@@ -1,4 +1,6 @@
+export { default as useAsyncEffect } from './useAsyncEffect';
 export { default as useDebounce } from './useDebounce';
+export { default as useIsMounted } from './useIsMounted';
 export { default as useKeybinding } from './useKeybinding';
 export * from './useKeybinding';
 export * from './useMediaQueries';
---
+++ src/shared/hooks/useAsyncEffect.ts
@@ -0,0 +1,38 @@
+import { useEffect } from 'react';
+
+const useAsyncEffect = <D = any>(
+  callback: (isMounted: () => boolean) => D | Promise<D>,
+  onDestroyOrDependencies: null | ((result?: D) => void) | any[] = [],
+  dependencies: any[] = [],
+) => {
+  let deps: any[];
+  let destroy: (result?: D) => void;
+
+  if (typeof onDestroyOrDependencies === 'function') {
+    destroy = onDestroyOrDependencies;
+    deps = dependencies;
+  } else {
+    deps = onDestroyOrDependencies || [];
+  }
+
+  useEffect(() => {
+    let result: D;
+    let mounted = true;
+
+    const maybePromise = callback(() => {
+      return mounted;
+    });
+
+    Promise.resolve(maybePromise).then((value) => {
+      result = value;
+    });
+
+    return () => {
+      mounted = false;
+      destroy(result);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, deps);
+};
+
+export default useAsyncEffect;
