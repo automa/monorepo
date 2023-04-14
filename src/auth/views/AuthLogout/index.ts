876a640e25f9a8588cdd3f707182cd9da38963f8
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2023-04-14 19:17:56 +0100

    Added auth logout

---
+++ src/auth/views/AuthLogout/index.ts
@@ -0,0 +1,2 @@
+export { default } from './AuthLogout';
+export * from './types';
---
+++ src/auth/views/AuthLogout/types.ts
@@ -0,0 +1,3 @@
+import { HTMLAttributes } from 'react';
+
+export interface AuthLogoutProps extends HTMLAttributes<HTMLDivElement> {}
