20bf1231a6fcdfa055cc3c5e19702363fbf7de47
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2023-04-30 12:48:55 +0200

    Handle repository rename event

--- packages/api/src/hooks/github/repository.ts
+++ packages/api/src/hooks/github/repository.ts
@@ -7,7 +7,7 @@ const archived: GithubEventActionHandler = async (app, body) => {
       is_archived: false,
       orgs: {
         provider_type: 'github',
-        provider_id: `${body.installation.account.id}`,
+        provider_id: `${body.organization.id}`,
       },
     },
     data: {
@@ -16,6 +16,10 @@ const archived: GithubEventActionHandler = async (app, body) => {
   });
 };
 
+// We are not handling the `repository.deleted` event because we will not
+// recieve it at all. Instead, we handle the change with the
+// `installation_repositories.removed` event.
+
 const privatized: GithubEventActionHandler = async (app, body) => {
   await app.prisma.repos.updateMany({
     where: {
@@ -23,7 +27,7 @@ const privatized: GithubEventActionHandler = async (app, body) => {
       is_private: false,
       orgs: {
         provider_type: 'github',
-        provider_id: `${body.installation.account.id}`,
+        provider_id: `${body.organization.id}`,
       },
     },
     data: {
@@ -39,7 +43,7 @@ const publicized: GithubEventActionHandler = async (app, body) => {
       is_private: true,
       orgs: {
         provider_type: 'github',
-        provider_id: `${body.installation.account.id}`,
+        provider_id: `${body.organization.id}`,
       },
     },
     data: {
@@ -48,6 +52,26 @@ const publicized: GithubEventActionHandler = async (app, body) => {
   });
 };
 
+const renamed: GithubEventActionHandler = async (app, body) => {
+  await app.prisma.repos.updateMany({
+    where: {
+      provider_id: `${body.repository.id}`,
+      orgs: {
+        provider_type: 'github',
+        provider_id: `${body.organization.id}`,
+      },
+    },
+    data: {
+      name: body.repository.name,
+    },
+  });
+};
+
+// We are not handling the `repository.transferred` event because we will not
+// recieve it when the new owner doesn't have the app installed. Instead, we
+// handle the change with the `installation_repositories.added` and the
+// `installation_repositories.removed` events.
+
 const unarchived: GithubEventActionHandler = async (app, body) => {
   await app.prisma.repos.updateMany({
     where: {
@@ -55,7 +79,7 @@ const unarchived: GithubEventActionHandler = async (app, body) => {
       is_archived: true,
       orgs: {
         provider_type: 'github',
-        provider_id: `${body.installation.account.id}`,
+        provider_id: `${body.organization.id}`,
       },
     },
     data: {
@@ -68,5 +92,6 @@ export default {
   archived,
   privatized,
   publicized,
+  renamed,
   unarchived,
 };
