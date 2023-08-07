f4a14f9ff8b1b816064a35917b6ea314e375cbdc
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2023-08-07 14:21:20 +0100

    Build other packages for testing

--- scripts/start-deps.sh
+++ scripts/start-deps.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 docker compose -f scripts/docker-compose.yml up -d --remove-orphans
+
+yarn workspace @automa/common build
+yarn workspace @automa/prisma build
+
+psql -U automa -W automa -h localhost -f db/migrations/00000000-init/up.sql
