62e5e2f3f980695d0214d179e8d00a6fd7ae0840
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2023-08-19 22:30:17 +0100

    Use github development app for local

---
+++ scripts/local.config.js
@@ -0,0 +1,55 @@
+module.exports = {
+  apps: [
+    {
+      name: 'prisma-dev',
+      cwd: 'packages/prisma',
+      script: 'yarn',
+      interpreter: '/bin/bash',
+      args: 'dev',
+    },
+    {
+      name: 'common-dev',
+      cwd: 'packages/common',
+      script: 'yarn',
+      interpreter: '/bin/bash',
+      args: 'dev',
+    },
+    {
+      name: 'common-graphql-dev',
+      watch: ['packages/api/src/graphql/schema'],
+      script: 'yarn',
+      interpreter: '/bin/bash',
+      args: 'generate-graphql',
+      autorestart: false,
+    },
+    {
+      name: 'api-dev',
+      cwd: 'packages/api',
+      script: 'yarn',
+      interpreter: '/bin/bash',
+      args: 'dev',
+    },
+    {
+      name: 'api-graphql-dev',
+      cwd: 'packages/api',
+      watch: ['src/graphql/schema'],
+      script: 'yarn',
+      interpreter: '/bin/bash',
+      args: 'copy-graphql',
+      autorestart: false,
+    },
+    {
+      name: 'api',
+      cwd: 'packages/api',
+      watch: ['build'],
+      script: 'build/index.js',
+    },
+    {
+      name: 'console',
+      cwd: 'packages/console',
+      script: 'yarn',
+      interpreter: '/bin/bash',
+      args: 'start',
+    },
+  ],
+};
