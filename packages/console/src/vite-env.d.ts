8b6bb774972e5667e4766809e0199a86e13856f6
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2023-10-27 13:46:09 +0100

    Add sentry proxy

--- packages/console/src/vite-env.d.ts
+++ packages/console/src/vite-env.d.ts
@@ -7,6 +7,7 @@ interface ImportMetaEnv {
   readonly VITE_SEGMENT_KEY: string;
   readonly VITE_SEGMENT_HOST: string;
   readonly VITE_SENTRY_DSN: string;
+  readonly VITE_SENTRY_HOST: string;
   readonly VITE_STATSIG_KEY: string;
 }
 
