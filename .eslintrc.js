4dfea7a577be79f86123036e33c6a9339f6fdab0
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2023-11-14 21:53:08 +0000

    Upgrade typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -30,6 +30,8 @@ module.exports = {
       },
     ],
     '@typescript-eslint/no-empty-interface': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': 'warn',
     '@typescript-eslint/ban-ts-comment': 'off',
     '@typescript-eslint/ban-types': [
       'error',
