5c744db837633e9784a9e0de0b12bab66083fae5
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2024-05-13 12:46:28 +0100

    Added env logic

---
+++ src/env.ts
@@ -0,0 +1,10 @@
+import pkg from '../package.json';
+
+export const environment = process.env.NODE_ENV || 'development';
+
+export const isTest = environment === 'test';
+export const isProduction = !isTest && environment !== 'development';
+
+export const product = 'marketing-next';
+export const service = 'marketing-next';
+export const version = pkg.version;
