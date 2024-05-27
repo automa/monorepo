34531b34a6be15213111f473dd1ff5dc181d8bbc
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2024-05-27 12:51:43 +0100

    Fix anchor with button component

--- src/shared/components/Button/Button.tsx
+++ src/shared/components/Button/Button.tsx
@@ -1,10 +1,48 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import { ButtonComponentProps } from './types';
 
 import { Container } from './Button.styles';
 
+const Anchor: React.FC<ButtonComponentProps> = ({
+  href,
+  anchor,
+  to,
+  link,
+  type,
+  disabled,
+  children,
+}) => {
+  if (disabled || (!href && !to) || type !== 'button') {
+    return children;
+  }
+
+  if (href) {
+    return (
+      <a href={href} {...anchor}>
+        {children}
+      </a>
+    );
+  }
+
+  if (to) {
+    return (
+      <Link to={to} {...link}>
+        {children}
+      </Link>
+    );
+  }
+
+  return null;
+};
+
+// TODO: Add loading spinner state and use it instead of disabled state
 const Button: React.FC<ButtonComponentProps> = ({
+  href,
+  anchor,
+  to,
+  link,
   variant,
   size,
   fullWidth,
@@ -14,16 +52,25 @@ const Button: React.FC<ButtonComponentProps> = ({
   ...props
 }) => {
   return (
-    <Container
-      $variant={variant}
-      $size={size}
-      $fullWidth={fullWidth}
+    <Anchor
+      href={href}
+      anchor={anchor}
+      to={to}
+      link={link}
       type={type}
       disabled={disabled}
-      {...props}
     >
-      {children}
-    </Container>
+      <Container
+        $variant={variant}
+        $size={size}
+        $fullWidth={fullWidth}
+        type={type}
+        disabled={disabled}
+        {...props}
+      >
+        {children}
+      </Container>
+    </Anchor>
   );
 };
 
