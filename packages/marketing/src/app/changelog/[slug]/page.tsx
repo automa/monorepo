8fc2a90a9e2d41197a7ab8a6839d933497093874
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2024-05-29 10:11:29 +0100

    Add metadata for changelog entries

--- packages/marketing/src/app/changelog/[slug]/page.tsx
+++ packages/marketing/src/app/changelog/[slug]/page.tsx
@@ -1,15 +1,33 @@
 import React from 'react';
 
+import { parseContent } from 'utils';
+
+import { ChangelogMatter } from '../types';
 import { listChangelogs } from '../utils';
 import Changelog from '../Changelog';
 
 export const generateStaticParams = listChangelogs;
 
-// TODO: Add metadata from frontmatter
-
-const ChangelogPage: React.FC<{
+type Props = {
   params: ReturnType<typeof generateStaticParams>[0];
-}> = ({ params: { slug } }) => {
+};
+
+export const generateMetadata = async ({ params: { slug } }: Props) => {
+  const file = listChangelogs().find((p) => p.slug === slug);
+
+  if (!file) {
+    return null;
+  }
+
+  const { frontmatter } = await parseContent<ChangelogMatter>(file.path);
+
+  return {
+    title: `${frontmatter.title} ‒ Automa`,
+    description: frontmatter.description,
+  };
+};
+
+const ChangelogPage: React.FC<Props> = ({ params: { slug } }) => {
   const file = listChangelogs().find((p) => p.slug === slug);
 
   if (!file) {
