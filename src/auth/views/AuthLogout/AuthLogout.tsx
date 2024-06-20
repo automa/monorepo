automa-demo/console

Improved import order

src/auth/views/AuthLogout/AuthLogout.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 
-import { useUser, useAuth } from 'auth/hooks';
 import { useAsyncEffect } from 'shared';
 
-import { logout } from '../../utils';
+import { useUser, useAuth, logout } from 'auth';
 
 import { AuthLogoutProps } from './types';
 

automa/monorepo

src/auth/views/AuthLogout/AuthLogout.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 
-import { useUser, useAuth } from 'auth/hooks';
 import { useAsyncEffect } from 'shared';
 
-import { logout } from '../../utils';
+import { logout, useAuth, useUser } from 'auth';
 
 import { AuthLogoutProps } from './types';
 