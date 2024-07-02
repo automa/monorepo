8d86b5fb5324eeaaff58bac5b385802fccdd8b9d
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2024-07-02 21:59:57 +0100

    Fix lint issues

--- src/store.ts
+++ src/store.ts
@@ -1,5 +1,5 @@
-import { combineSlices, configureStore } from '@reduxjs/toolkit';
 import { useDispatch, useSelector } from 'react-redux';
+import { combineSlices, configureStore } from '@reduxjs/toolkit';
 
 import { isProduction } from 'env';
 
