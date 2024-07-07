d115954701e756acf95e9dd2b0ed1f014e6d8bb5
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2024-07-07 15:55:44 +0100

    Add ref forwarding to Button

--- src/shared/components/Button/Button.tsx
+++ src/shared/components/Button/Button.tsx
@@ -7,41 +7,47 @@ import { ButtonComponentProps } from './types';
 import { Container } from './Button.styles';
 
 // TODO: Add loading spinner state and use it instead of disabled state
-const Button: React.FC<ButtonComponentProps> = ({
-  href,
-  blank,
-  anchor,
-  to,
-  link,
-  variant,
-  size,
-  fullWidth,
-  type = 'button',
-  disabled,
-  children,
-  ...props
-}) => {
-  return (
-    <Anchor
-      href={href}
-      blank={blank}
-      anchor={anchor}
-      to={to}
-      link={link}
-      disabled={disabled || type !== 'button'}
-    >
-      <Container
-        $variant={variant}
-        $size={size}
-        $fullWidth={fullWidth}
-        type={type}
-        disabled={disabled}
-        {...props}
+const Button = React.forwardRef<HTMLButtonElement, ButtonComponentProps>(
+  function Button(
+    {
+      href,
+      blank,
+      anchor,
+      to,
+      link,
+      variant,
+      size,
+      fullWidth,
+      type = 'button',
+      disabled,
+      children,
+      ...props
+    },
+    ref,
+  ) {
+    return (
+      <Anchor
+        href={href}
+        blank={blank}
+        anchor={anchor}
+        to={to}
+        link={link}
+        disabled={disabled || type !== 'button'}
       >
-        {children}
-      </Container>
-    </Anchor>
-  );
-};
+        <Container
+          $variant={variant}
+          $size={size}
+          $fullWidth={fullWidth}
+          type={type}
+          disabled={disabled}
+          ref={ref}
+          {...props}
+        >
+          {children}
+        </Container>
+      </Anchor>
+    );
+  },
+);
 
 export default Button;
