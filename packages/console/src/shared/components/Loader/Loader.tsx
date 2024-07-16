066f8d12ba47992aeeb48d29eae88cffd7fc0f21
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2024-07-16 12:20:16 +0100

    Added loading animation

--- packages/console/src/shared/components/Loader/Loader.tsx
+++ packages/console/src/shared/components/Loader/Loader.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 
+import Loading from 'assets/loading.webp';
+
 import { LoaderProps } from './types';
 
 import { Container } from './Loader.styles';
 
 const Loader: React.FC<LoaderProps> = ({ ...props }) => {
-  return <Container {...props}>Loading...</Container>;
+  return (
+    <Container {...props}>
+      <img className="size-20" src={Loading} alt="loading" />
+    </Container>
+  );
 };
 
 export default Loader;
