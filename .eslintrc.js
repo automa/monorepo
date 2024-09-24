65bef0308c672db63be796f4fb467b10f7e04261
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2024-09-24 20:52:28 +0100

    Add eslint rules for non-prettier available formatting

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
   plugins: ['simple-import-sort'],
   processor: '@graphql-eslint/graphql',
   rules: {
+    quotes: ['warn', 'single', { avoidEscape: true }],
     'no-empty': [
       'error',
       {
@@ -38,6 +39,7 @@ module.exports = {
       },
     ],
     'react/react-in-jsx-scope': 'off',
+    'react/jsx-curly-brace-presence': 'warn',
     'simple-import-sort/imports': [
       'warn',
       {
