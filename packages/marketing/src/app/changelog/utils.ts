b6d655cf181abf5a6afb418820bcef220677ad97
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2025-05-27 20:04:47 +0530

    Cache the result of static params

--- packages/marketing/src/app/changelog/utils.ts
+++ packages/marketing/src/app/changelog/utils.ts
@@ -1,13 +1,22 @@
 import { readdirSync } from 'fs';
 import { join } from 'path';
 
-import { contentPath } from 'utils';
+import { contentPath, isBuildTime } from 'utils';
 
-export const listChangelogs = () =>
-  readdirSync(contentPath('changelogs'))
+let listChangelogsCache: { date: string; slug: string; path: string }[];
+
+export const listChangelogs = () => {
+  if (isBuildTime && listChangelogsCache) {
+    return listChangelogsCache;
+  }
+
+  listChangelogsCache = readdirSync(contentPath('changelogs'))
     .map((name) => ({
       path: join(contentPath('changelogs'), name),
       ...(name.match(/^(?<date>\d{4}-\d{2}-\d{2})-(?<slug>[a-z\d-]+)\.mdx$/)!
         .groups as { date: string; slug: string }),
     }))
     .reverse();
+
+  return listChangelogsCache;
+};
--- packages/marketing/src/utils.ts
+++ packages/marketing/src/utils.ts
@@ -5,6 +5,8 @@ import { compileMDX } from 'next-mdx-remote/rsc';
 
 import { common } from 'mdx-components';
 
+export const isBuildTime = process.env.NODE_ENV === 'production';
+
 export const contentPath = (type: string) =>
   join(process.cwd(), 'src', 'content', type);
 
