279cbdfe7de94b17b77b47c1ed6136dabee0e30b
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2025-06-03 19:46:38 +0530

    Write manifesto

--- packages/marketing/src/app/manifesto/page.styles.ts
+++ packages/marketing/src/app/manifesto/page.styles.ts
@@ -7,4 +7,4 @@ export const Container = tw.div`mx-auto max-w-3xl py-6 text-lg md:py-12 lg:py-16
 export const Title = tw(Typography).attrs({
   variant: 'title2',
   align: 'center',
-})`text-3xl md:text-4xl max-w-64 sm:max-w-3xl`;
+})`text-3xl md:text-4xl max-w-64 sm:max-w-3xl mb-12 lg:mb-20 mx-auto`;
