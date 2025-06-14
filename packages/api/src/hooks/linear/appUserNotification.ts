64211039294b3ce2fb4e75387a91d78db5790b2f
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2025-06-15 04:18:49 +0530

    No double task from linear comment

--- packages/api/src/hooks/linear/appUserNotification.ts
+++ packages/api/src/hooks/linear/appUserNotification.ts
@@ -32,38 +32,13 @@ const issueAssignedToYou: LinearEventActionHandler<{
     ...body.notification,
   });
 
-const issueCommentMention: LinearEventActionHandler<{
-  organizationId: string;
-  notification: {
-    issue: {
-      id: string;
-      team: {
-        id: string;
-        key: string;
-        name: string;
-      };
-    };
-    comment: {
-      id: string;
-      body: string;
-    };
-    parentCommentId?: string;
-    actor: {
-      id: string;
-      name: string;
-      email: string;
-    };
-  };
-}> = (app, body) =>
-  handleMention(app, {
-    organizationId: body.organizationId,
-    ...body.notification,
-  });
+// We are not handling the `appUserNotification.issueCommentMention` event as it
+// is handled by the `comment.create` event (so that unmentioned comments work too).
+// If you want to reinstate, revert the commit that added this comment.
 
 // TODO: Handle the following:
 // - issueMention
 // - issueUnassignedFromYou (not working)
 export default {
   issueAssignedToYou,
-  issueCommentMention,
 };
