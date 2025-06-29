66e8e64390d9b90393c4ed450f3eaf3fc68d7b94
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2025-06-29 15:16:53 +0530

    Tests for slices

---
+++ packages/console/src/app/slices/app.test.ts
@@ -0,0 +1,31 @@
+import { describe, expect, it } from 'vitest';
+
+import { App } from 'gql/graphql';
+
+import reducer, { setApp, unsetApp } from './app';
+
+const dummyApp: App = {
+  client_uri: 'uri',
+  cloud: true,
+  integrations: {
+    github: true,
+    gitlab: false,
+    jira: false,
+    linear: false,
+    slack: true,
+  },
+};
+
+describe('appReducer', () => {
+  it('should return the initial state', () => {
+    expect(reducer(undefined, { type: '@@INIT' })).toEqual({ app: null });
+  });
+
+  it('should set app', () => {
+    expect(reducer({ app: null }, setApp(dummyApp))).toEqual({ app: dummyApp });
+  });
+
+  it('should unset app', () => {
+    expect(reducer({ app: dummyApp }, unsetApp())).toEqual({ app: null });
+  });
+});
